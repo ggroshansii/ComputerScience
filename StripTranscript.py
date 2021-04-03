715f984b36b19e5c74e64f9d38d188b5911cb23a
groshansii <garthgroshansii@gmail.com>
2021-04-03 10:16:18 -0400

    Early Computing

--- StripTranscript.py
+++ StripTranscript.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python3
-fileName = open("/home/gh0st/Documents/transcript.txt", "w+")
+fileName1 =  open("/home/gh0st/Documents/transcript.txt", "r")
 
-for line in fileName.readlines():
-    fileName.write(line.rstrip())
+strippedSentences = []
 
-for line in fileName:
+for line in fileName1.readlines():
+    strippedSentences.append(line.rstrip())
+
+
+fileName2 = open("/home/gh0st/Documents/transcript.txt", "w")
+
+for i in strippedSentences:
+    fileName2.write(i + " ")
+
+
+fileName3 = open("/home/gh0st/Documents/transcript.txt", "r")
+
+for line in fileName3:
     print(line)
