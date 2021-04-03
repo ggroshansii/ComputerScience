ce028799a436a684c57861a72263e56ddbc37253
groshansii <garthgroshansii@gmail.com>
2021-04-03 10:17:49 -0400

    script that reads/writes .txt and removes newline characters

--- StripTranscript.py
+++ StripTranscript.py
@@ -6,12 +6,10 @@ strippedSentences = []
 for line in fileName1.readlines():
     strippedSentences.append(line.rstrip())
 
-
 fileName2 = open("/home/gh0st/Documents/transcript.txt", "w")
 
 for i in strippedSentences:
     fileName2.write(i + " ")
-
 
 fileName3 = open("/home/gh0st/Documents/transcript.txt", "r")
 
